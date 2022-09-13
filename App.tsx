2b738875a994c4113c8cf58aa7f42f51b945d7fc
Bruno <bruno.pacheco@tusta.com.br>
2022-09-12 22:25:49 -0300

    feat: create buttons and pass title as prop to button

--- App.tsx
+++ App.tsx
@@ -1,13 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello react native!</Text>
+      <Text>
+        Hello react native!
+      </Text>
+
+      <Button title="Send 1" />
+      <Button title="Send 2" />
+      <Button title="Send 3" />
+      <Button title="Hello world" />
       <StatusBar style="auto" />
     </View>
   );
+}
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +37,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
