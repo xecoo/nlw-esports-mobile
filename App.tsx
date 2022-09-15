d573743ee57f20241bb5a26e9b621908f81dad19
Bruno <bruno.pacheco@tusta.com.br>
2022-09-15 07:48:40 -0300

    feat: create background img as component

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
 import { View } from 'react-native';
 
+import { Background } from './src/components/Background/index';
+
 export default function App() {
   return (
     <View>
-
+      <Background>
+        
+      </Background>
     </View>
   );
 }
