ef1214ca44ffc371cd50a1f67d2357e90f8677b1
Bruno <bruno.pacheco@tusta.com.br>
2022-09-17 09:34:54 -0300

    feat: insert logo on home using require instead import

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { View, Image } from 'react-native';
 import { styles } from './styles';
 
-import { logoImg } from '../../assets/background-galaxy.png'
-
 export function Home() {
     return (
         <View style={styles.container}>
             <Image
-                source={logoImg}
+                source={require ('../../assets/logo-nlw-esports.png')}
+                style={styles.logo}
             />
         </View>
     )
