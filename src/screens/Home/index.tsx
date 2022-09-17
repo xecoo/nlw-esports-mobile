3cb14419ed21db1b46a6b4235bc72e5251b7dde5
Bruno <bruno.pacheco@tusta.com.br>
2022-09-17 09:36:36 -0300

    feat: insert logo on home using import without braces

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { View, Image } from 'react-native';
 import { styles } from './styles';
 
+import logoImg from '../../assets/logo-nlw-esports.png';
+
 export function Home() {
     return (
         <View style={styles.container}>
             <Image
-                source={require ('../../assets/logo-nlw-esports.png')}
+                source={logoImg}
                 style={styles.logo}
             />
         </View>
