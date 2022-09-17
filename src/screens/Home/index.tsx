6bd777e96841e1ffdf6765773b63710f0af193a4
Bruno <bruno.pacheco@tusta.com.br>
2022-09-17 08:58:03 -0300

    feat: insert logo on a app.tsx

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import {View } from 'react-native';
-import {styles} from './styles';
+import { View, Image } from 'react-native';
+import { styles } from './styles';
+
+import { logoImg } from '../../assets/background-galaxy.png'
 
 export function Home() {
     return (
         <View style={styles.container}>
-
+            <Image
+                source={logoImg}
+            />
         </View>
     )
 }
