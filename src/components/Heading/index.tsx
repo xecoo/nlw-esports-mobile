6e59f0fc3823a92e6f998fee68c3c9de65966ded
Bruno <bruno.pacheco@tusta.com.br>
2022-09-17 10:04:34 -0300

    feat:finishing heading and import on home

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,12 +1,16 @@
 import { View, Text } from 'react-native';
 import { Props } from './iHeading';
-
 import { styles } from './styles';
 
-export function Heading({title, subtitle, ...rest}: Props) {
+export function Heading({ title, subtitle, ...rest }: Props) {
   return (
     <View style={styles.container} {...rest}>
-
+      <Text style={styles.title}>
+        {title}
+      </Text>
+      <Text style={styles.subtitle}>
+        {subtitle}
+      </Text>
     </View>
   );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { View, Image } from 'react-native';
 import { styles } from './styles';
-
 import logoImg from '../../assets/logo-nlw-esports.png';
+import { Heading } from '../../components/Heading';
 
 export function Home() {
-    return (
-        <View style={styles.container}>
-            <Image
-                source={logoImg}
-                style={styles.logo}
-            />
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Image
+        source={logoImg}
+        style={styles.logo}
+      />
+
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar ..."
+      />
+    </View>
+  )
 }
